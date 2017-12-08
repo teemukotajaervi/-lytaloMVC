07c8cd0a52c96f8e44ac4060012eb7b902bcb16d
Teemu Kotajärvi <teemu.kotajarvi@stud.pointcollege.fi>
2017-12-08 12:56:18 +0200

    Luotu Entiteetti malli.

---
+++ ÄlytaloMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ÄlytaloMVC.Startup))]
+namespace ÄlytaloMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
